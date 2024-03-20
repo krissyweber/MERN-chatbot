d5bb289c4e8f9b0a804115d94aaa31b17c015ae0
krissyweber <122844703+krissyweber@users.noreply.github.com>
2024-03-20 18:21:41 -0400

    first commit

---
+++ backend/dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("can not connect to MongoDB");
+    }
+}
+async function disconnectFromDB() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("could not disconnect from MongoDB");
+    }
+}
+export { connectToDatabase, disconnectFromDB };
+//# sourceMappingURL=connection.js.map
