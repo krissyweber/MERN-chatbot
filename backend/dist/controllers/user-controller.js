ae961c4194ff05256805493a66386ccfa852f5b3
krissyweber <122844703+krissyweber@users.noreply.github.com>
2024-05-13 20:45:33 -0400

    signup/login

---
+++ backend/dist/controllers/user-controller.js
@@ -0,0 +1,49 @@
+import User from "../models/User.js";
+import { hash, compare } from 'bcrypt';
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//user signup
+export const userSignUp = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const exsistingUser = await User.findOne({ email });
+        if (exsistingUser)
+            return res.status(401).send("User already registered");
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(201).json({ message: "ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//user login
+export const userLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("incorrect password");
+        }
+        return res.status(201).json({ message: "ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//# sourceMappingURL=user-controller.js.map
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,3 +1,64 @@
-export const getAllUsers = ()=>{
+import { NextFunction, Request, Response,  } from "express";
+import User from "../models/User.js";
+import { hash, compare } from 'bcrypt';
 
+export const getAllUsers = async(
+    req:Request, 
+    res:Response,
+    next:NextFunction
+    )=>{
+    try {
+        const users = await User.find();
+        return res.status(200).json({message: "ok", users});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "ERROR", cause: error.message});
+    }
+        
+};
+//user signup
+export const userSignUp = async(
+    req:Request, 
+    res:Response,
+    next:NextFunction
+    )=>{
+    try {
+
+        const {name, email, password} = req.body;
+        const exsistingUser = await User.findOne({email});
+        if(exsistingUser) return res.status(401).send("User already registered");
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword});
+        await user.save();
+        return res.status(201).json({message: "ok", id: user._id.toString() });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "ERROR", cause: error.message});
+    }
+        
+};
+
+//user login
+export const userLogin = async(
+    req:Request, 
+    res:Response,
+    next:NextFunction
+    )=>{
+    try {
+        const { email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(403).send("incorrect password");
+        }
+
+    return res.status(201).json({message: "ok", id: user._id.toString() });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "ERROR", cause: error.message});
+    }
+        
 };
